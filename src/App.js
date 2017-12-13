e85f035cdb35ace88513aeeb17aa89e3f87b1f71
Juan Rojas <jmr5150@gmail.com>
2017-12-12 23:33:27 -0600

    Rename project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import "./app.scss"
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="main-wrapper">
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
