48a76cf6f166346d25efb9f38a002f890468a655
Juan Rojas <jmr5150@gmail.com>
2017-12-18 18:22:09 -0600

    Adds horrible header to starting location component

--- src/components/StartingLocationInput.js
+++ src/components/StartingLocationInput.js
@@ -4,7 +4,12 @@ import LocationAutocomplete from 'location-autocomplete'
 import Input from './Input.js'
 
 const StartingLocationInput = (props) => {
-  return props.children
+  return (
+    <div>
+      <h1>Starting Location</h1>
+      {props.children}
+    </div>
+  )
 }
 
 export default StartingLocationInput
